67d02c7bb9991c5cd148359132cbd3b1c6fb5297
Movarind <u17045780@tuks.co.za>
2019-07-24 15:17:17 +0200

    Middle Div Against NavBar

---
+++ Gumtion/Gumtion/Controllers/RawMaterialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gumtion.Controllers
+{
+    public class RawMaterialController : Controller
+    {
+        // GET: RawMaterial
+        public ActionResult RawMaterialIndex()
+        {
+            return View();
+        }
+    }
+}
